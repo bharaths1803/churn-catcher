573eb3e5cff9f6fb9e8345098f52751273890e9f
bharaths1803 <bharaths14051803@gmail.com>
2025-04-11 01:28:39 +0530

    Some more changes

---
+++ backend/download_models.py
@@ -0,0 +1,19 @@
+import  zipfile, gdown, os
+
+# Google Drive file ID
+file_id = "1bhxhNqkjRCw4NHF0SvHcHc18MrOrvy1C"
+
+# Output zip path
+output = "models_bundle_downloaded.zip"
+
+# Download from Google Drive
+gdown.download(f"https://drive.google.com/uc?id={file_id}", output, quiet=False)
+
+# Extract
+with zipfile.ZipFile(output, 'r') as zip_ref:
+    zip_ref.extractall("ml/new_models")  # Adjust this if needed
+
+print("✅ Models downloaded and extracted successfully.")
+
+# Optional: remove the zip file
+os.remove(output)
